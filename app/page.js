91d5a751cc4e5d97debbe48501f576d33eab162c
Div685 <divyeshpatel685@gmail.com>
2024-10-15 04:13:31 +0545

    Add Gift Category component

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import BannerCarosel from "./components/BannerCarosel";
 import HeroSection from "./components/HeroSection";
 import AllCategories from "./components/AllCategories/AllCategories";
 import CardsCategories from "./components/CardsCategories/CardsCategories";
+import GiftCategories from "./components/GiftCategories/GiftCategories";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <HeroSection />
       <AllCategories />
       <CardsCategories />
+      <GiftCategories />
       <h1>Printing Web App</h1>
     </div>
   );
