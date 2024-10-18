a3cfec0d9116446835947f5c18916293efbd1b9a
Div685 <divyeshpatel685@gmail.com>
2024-10-19 00:54:57 +0300

    Remove comments from page file

--- app/page.js
+++ app/page.js
@@ -1,6 +1,3 @@
-import Image from "next/image";
-import Header from "./components/Header";
-import BannerCarosel from "./components/BannerCarosel";
 import HeroSection from "./components/HeroSection";
 import AllCategories from "./components/AllCategories/AllCategories";
 import CardsCategories from "./components/CardsCategories/CardsCategories";
@@ -9,8 +6,6 @@ import GiftCategories from "./components/GiftCategories/GiftCategories";
 export default function Home() {
   return (
     <div className="">
-      {/* <Header />
-      <BannerCarosel /> */}
       <HeroSection />
       <AllCategories />
       <CardsCategories />
