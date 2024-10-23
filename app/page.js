c4359a6437ba5021486bde344831e38299882eeb
marvin-arthur <marvinssembajjwe@gmail.com>
2024-10-23 19:19:49 +0300

    adjustment to footer

--- app/page.js
+++ app/page.js
@@ -3,8 +3,6 @@ import Header from "./components/Header";
 import BannerCarosel from "./components/BannerCarosel";
 import HeroSection from "./components/HeroSection";
 import Footer from "./components/Footer";
-import DiscountCodes from "./components/DiscountCodes";
-import Faq from "./components/Faq";
 
 export default function Home() {
   return (
@@ -13,7 +11,6 @@ export default function Home() {
       <BannerCarosel />
       <HeroSection />
       <h1>Printing Web App</h1>
-      <Faq />
       <Footer />
     </div>
   );
