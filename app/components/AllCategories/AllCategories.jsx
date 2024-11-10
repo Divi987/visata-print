63a2ef654de424ce330a2b76541cc13bd39d80e3
Div685 <divyeshpatel685@gmail.com>
2024-11-10 16:08:05 +0300

    Add Api req to fetch data from DB

--- app/components/AllCategories/AllCategories.jsx
+++ app/components/AllCategories/AllCategories.jsx
@@ -2,52 +2,61 @@
 import AllCategory from "./AllCategory";
 import style from "../navbarStyles.module.css";
 import { useState } from "react";
+import { useRecoilValueLoadable } from "recoil";
+import { categoryStateSelector } from "@/lib/recoil/selectors/selectors";
+import { fetchCategories } from "@/lib/api/fetchRequest";
 
 export default function AllCategories() {
-  let items = {
-    item: [
-      {
-        img: "https://cms.cloudinary.vpsvc.com/images/c_scale,dpr_auto,f_auto,fl_progressive,w_600/legacy_dam/en-us/S001707823/MXP-26978-CatZone-Deals-tile-001",
-        p: "Signs, Banners & Poster",
-      },
-      {
-        img: "https://cms.cloudinary.vpsvc.com/images/c_scale,dpr_auto,f_auto,fl_progressive,w_600/legacy_dam/en-us/S001705599/MXP26584-Jan8-Homepage-Category-Tile-BC-002?cb=8ec2cd87bd754846557e35a61359fcca3f1f0491",
-        p: "Label, Sticker & Packaging",
-      },
-      {
-        img: "https://cms.cloudinary.vpsvc.com/images/c_scale,dpr_auto,f_auto,fl_progressive,w_600/legacy_dam/en-us/S001704789/MXP26584-Jan8-Homepage-Category-Tile-Postcards-001?cb=cfa8d1b638701affc076140a9ba5892c2a9a95e7",
-        p: "Home & Gifts",
-      },
-      {
-        img: "https://cms.cloudinary.vpsvc.com/images/c_scale,dpr_auto,f_auto,fl_progressive,w_600/legacy_dam/en-us/S001705594/MXP26584-Jan8-Homepage-Category-Tile-IA-002?cb=fe4051222ffe6ef77b02c6827debcdeaa45c4873",
-        p: "Celebrations, Invitations & Stationaries",
-      },
-      {
-        img: "https://cms.cloudinary.vpsvc.com/images/c_scale,dpr_auto,f_auto,fl_progressive,w_600/legacy_dam/en-us/S001705599/MXP26584-Jan8-Homepage-Category-Tile-BC-002?cb=8ec2cd87bd754846557e35a61359fcca3f1f0491",
-        p: "Label, Sticker & Packaging",
-      },
-      {
-        img: "https://cms.cloudinary.vpsvc.com/images/c_scale,dpr_auto,f_auto,fl_progressive,w_600/legacy_dam/en-us/S001704789/MXP26584-Jan8-Homepage-Category-Tile-Postcards-001?cb=cfa8d1b638701affc076140a9ba5892c2a9a95e7",
-        p: "Home & Gifts",
-      },
-      {
-        img: "https://cms.cloudinary.vpsvc.com/images/c_scale,dpr_auto,f_auto,fl_progressive,w_600/legacy_dam/en-us/S001705594/MXP26584-Jan8-Homepage-Category-Tile-IA-002?cb=fe4051222ffe6ef77b02c6827debcdeaa45c4873",
-        p: "Celebrations, Invitations & Stationaries",
-      },
-    ],
-  };
+  const categoriesLoadable = useRecoilValueLoadable(categoryStateSelector);
 
-  let [current, setCurrent] = useState(0);
+  // if (categoriesLoadable.state === 'loading') return <p>Loading categories...</p>;
+  if (categoriesLoadable.state === 'hasError') return <p>Error loading categories</p>;
+
+  let items = categoriesLoadable.contents;
+  //  {
+  //   item: [
+  //     {
+  //       img: "https://cms.cloudinary.vpsvc.com/images/c_scale,dpr_auto,f_auto,fl_progressive,w_600/legacy_dam/en-us/S001707823/MXP-26978-CatZone-Deals-tile-001",
+  //       p: "Signs, Banners & Poster",
+  //     },
+  //     {
+  //       img: "https://cms.cloudinary.vpsvc.com/images/c_scale,dpr_auto,f_auto,fl_progressive,w_600/legacy_dam/en-us/S001705599/MXP26584-Jan8-Homepage-Category-Tile-BC-002?cb=8ec2cd87bd754846557e35a61359fcca3f1f0491",
+  //       p: "Label, Sticker & Packaging",
+  //     },
+  //     {
+  //       img: "https://cms.cloudinary.vpsvc.com/images/c_scale,dpr_auto,f_auto,fl_progressive,w_600/legacy_dam/en-us/S001704789/MXP26584-Jan8-Homepage-Category-Tile-Postcards-001?cb=cfa8d1b638701affc076140a9ba5892c2a9a95e7",
+  //       p: "Home & Gifts",
+  //     },
+  //     {
+  //       img: "https://cms.cloudinary.vpsvc.com/images/c_scale,dpr_auto,f_auto,fl_progressive,w_600/legacy_dam/en-us/S001705594/MXP26584-Jan8-Homepage-Category-Tile-IA-002?cb=fe4051222ffe6ef77b02c6827debcdeaa45c4873",
+  //       p: "Celebrations, Invitations & Stationaries",
+  //     },
+  //     {
+  //       img: "https://cms.cloudinary.vpsvc.com/images/c_scale,dpr_auto,f_auto,fl_progressive,w_600/legacy_dam/en-us/S001705599/MXP26584-Jan8-Homepage-Category-Tile-BC-002?cb=8ec2cd87bd754846557e35a61359fcca3f1f0491",
+  //       p: "Label, Sticker & Packaging",
+  //     },
+  //     {
+  //       img: "https://cms.cloudinary.vpsvc.com/images/c_scale,dpr_auto,f_auto,fl_progressive,w_600/legacy_dam/en-us/S001704789/MXP26584-Jan8-Homepage-Category-Tile-Postcards-001?cb=cfa8d1b638701affc076140a9ba5892c2a9a95e7",
+  //       p: "Home & Gifts",
+  //     },
+  //     {
+  //       img: "https://cms.cloudinary.vpsvc.com/images/c_scale,dpr_auto,f_auto,fl_progressive,w_600/legacy_dam/en-us/S001705594/MXP26584-Jan8-Homepage-Category-Tile-IA-002?cb=fe4051222ffe6ef77b02c6827debcdeaa45c4873",
+  //       p: "Celebrations, Invitations & Stationaries",
+  //     },
+  //   ],
+  // };
+
+  // let [current, setCurrent] = useState(0);
 
 //   Use page % instead of items length
   let previousSlide = () => {
-    if (current === 0) setCurrent(items.item.length - 5);
-    else setCurrent(current - 1);
+    // if (current === 0) setCurrent(items.item.length - 5);
+    // else setCurrent(current - 1);
   };
 
   let nextSlide = () => {
-    if (current === items.item.length - 5) setCurrent(0);
-    else setCurrent(current + 1);
+    // if (current === items.item.length - 5) setCurrent(0);
+    // else setCurrent(current + 1);
   };
 
 
@@ -61,7 +70,7 @@ export default function AllCategories() {
         </div>
         <div className="flex space-x-4 overflow-x-scroll overscroll-x-none no-scrollbar">
           <button
-            disabled={current === 0}
+            // disabled={current === 0}
             onClick={previousSlide}
             type="button"
             className="absolute top-0 start-0 left-2 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
@@ -76,18 +85,18 @@ export default function AllCategories() {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M5 1 1 5l4 4"
                 />
               </svg>
               <span className="sr-only">Previous</span>
             </span>
           </button>
-          <AllCategory items={items.item} current={current} />
+          <AllCategory items={categoriesLoadable} current={0} />
           <button
-          disabled={current === (items.item.length -1)}
+          // disabled={current === (items.item.length -1)}
           onClick={nextSlide}
             type="button"
             className="absolute top-0 end-0 right-2 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
@@ -102,9 +111,9 @@ export default function AllCategories() {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="m1 9 4-4-4-4"
                 />
               </svg>
--- app/components/AllCategories/AllCategory.jsx
+++ app/components/AllCategories/AllCategory.jsx
@@ -1,22 +1,80 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function AllCategory({ items, current }) {
-    
+
+  if (items.state === 'loading') return  (
+<>
+    <div className="m-2 w-3/12 md:w-2/12 lg:w-[14%] overflow-hidden shrink-0 bg-red ">
+    <div className="flex flex-col ">
+    <div className="bg-[#f3f3f3] rounded-full overflow-hidden h-[100px] md:h-[120px] lg:h-[140px] ">
+    </div>
+    <div className="text-center p-4 lg:text-[16px] font-bold"></div>    
+    </div>
+  </div>
+   <div className="m-2 w-3/12 md:w-2/12 lg:w-[14%] overflow-hidden shrink-0 bg-red ">
+   <div className="flex flex-col ">
+   <div className="bg-[#f3f3f3] rounded-full overflow-hidden h-[100px] md:h-[120px] lg:h-[140px] ">
+   </div>
+   <div className="text-center p-4 lg:text-[16px] font-bold"></div>    
+   </div>
+ </div>
+  <div className="m-2 w-3/12 md:w-2/12 lg:w-[14%] overflow-hidden shrink-0 bg-red ">
+  <div className="flex flex-col ">
+  <div className="bg-[#f3f3f3] rounded-full overflow-hidden h-[100px] md:h-[120px] lg:h-[140px] ">
+  </div>
+  <div className="text-center p-4 lg:text-[16px] font-bold"></div>    
+  </div>
+</div>
+</>
+  )
   return (
     <>
-      {items.map((item) => {
+      {items.contents.data?.map((item) => {
         return (
           <>
-            <figure className="flex flex-col shrink-0 rounded-full bg-red m-2 w-3/12 md:w-2/12 lg:w-[14%]"
-            style={{
-                transform: `translateX(-${current * 100}%)`,
-              }}
+            <Link
+            key={item._id}
+              href={item.slug}
+              className="m-2 w-3/12 md:w-2/12 lg:w-[14%] overflow-hidden shrink-0 bg-red "
             >
-              <span className="bg-[#f3f3f3] rounded-full overflow-hidden">
-                <img src={item.img} alt="img" srcset="" className="" />
-              </span>
-              <figcaption className="text-center pt-4 lg:text-[16px] font-bold">
-                {item.p}
-              </figcaption>
-            </figure>
+              <figure
+                
+                className="flex flex-col "
+                style={{
+                  transform: `translateX(-${current * 100}%)`,
+                }}
+              >
+                <span className="bg-[#f3f3f3] rounded-full overflow-hidden h-[100px] md:h-[120px] lg:h-[140px] xl:h-[160px] ">
+                  {/* <img
+                    src={
+                      item.image
+                        ? item.image?.url
+                        : "https://cms.cloudinary.vpsvc.com/images/c_scale,dpr_auto,f_auto,fl_progressive,w_600/legacy_dam/en-us/S001705599/MXP26584-Jan8-Homepage-Category-Tile-BC-002?cb=8ec2cd87bd754846557e35a61359fcca3f1f0491"
+                    }
+                    alt="img"
+                    srcset=""
+                    className="object-cover h-full md:h-full md:h-[128px]"
+                  /> */}
+                  <Image
+                  src={
+                    item.image
+                      ? 
+                      item.image?.url
+                      : "https://cms.cloudinary.vpsvc.com/images/c_scale,dpr_auto,f_auto,fl_progressive,w_600/legacy_dam/en-us/S001705599/MXP26584-Jan8-Homepage-Category-Tile-BC-002?cb=8ec2cd87bd754846557e35a61359fcca3f1f0491"
+                  }
+                  alt="img"
+                  srcSet=""
+                  className="object-cover h-full"
+                  height={item.image ? item.image?.height : 150}
+                  width={item.image ? item.image?.width : 150}
+                  />
+                </span>
+                <figcaption className="text-center pt-4 lg:text-[16px] font-bold">
+                  {item.name}
+                </figcaption>
+              </figure>
+            </Link>
           </>
         );
       })}
