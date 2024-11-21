8dd764d8808d5a9a7a00d05c1c871bbcf0edfbe6
Div685 <divyeshpatel685@gmail.com>
2024-11-22 01:05:17 +0300

    Add EditorProject Page

--- app/editor/[projectId]/page.jsx
+++ app/editor/[projectId]/page.jsx
@@ -1,5 +1,24 @@
-export default function ({ params }) {
-    return (
-        <></>
-    )
+import { Editor } from "@/features/editors/components/editor";
+import { useGetProject } from "@/features/projects/api/use-get-project";
+import { Loader } from "lucide-react";
+
+export default function EditorProjectIdPage ({ params }) {
+    const data = {
+        id: 11
+    }
+    // const { 
+    //     data, 
+    //     isLoading, 
+    //     isError
+    //   } = useGetProject(params);
+    
+    //   if (isLoading || !data) {
+    //     return (
+    //       <div className="h-full flex flex-col items-center justify-center">
+    //         <Loader className="size-6 animate-spin text-muted-foreground" />
+    //       </div>
+    //     );
+    //   }
+    
+    return <Editor initialData={data} />
 }
