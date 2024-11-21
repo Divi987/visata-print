849e9009a0e5438424080cf76030094552022dab
Div685 <divyeshpatel685@gmail.com>
2024-11-22 00:53:49 +0300

    Add auto-resize, clipboard hooks
    - and canvas event hooks to work with editor

---
+++ features/editors/hooks/use-canvas-events.js
@@ -0,0 +1,44 @@
+import { fabric } from "fabric";
+import { useEffect } from "react";
+
+export const useCanvasEvents = ({
+  save,
+  canvas,
+  setSelectedObjects,
+  clearSelectionCallback,
+}) => {
+  useEffect(() => {
+    if (canvas) {
+      canvas.on("object:added", () => save());
+      canvas.on("object:removed", () => save());
+      canvas.on("object:modified", () => save());
+      canvas.on("selection:created", (e) => {
+        setSelectedObjects(e.selected || []);
+      });
+      canvas.on("selection:updated", (e) => {
+        setSelectedObjects(e.selected || []);
+      });
+      canvas.on("selection:cleared", () => {
+        setSelectedObjects([]);
+        clearSelectionCallback?.();
+      });
+    }
+
+    return () => {
+      if (canvas) {
+        canvas.off("object:added");
+        canvas.off("object:removed");
+        canvas.off("object:modified");
+        canvas.off("selection:created");
+        canvas.off("selection:updated");
+        canvas.off("selection:cleared");
+      }
+    };
+  },
+  [
+    save,
+    canvas,
+    clearSelectionCallback,
+    setSelectedObjects // No need for this, this is from setState
+  ]);
+};
