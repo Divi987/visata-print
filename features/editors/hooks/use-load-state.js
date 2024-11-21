de16155e12d4d31817e1920ec1cbbc0bdfc10f1b
Div685 <divyeshpatel685@gmail.com>
2024-11-22 00:58:00 +0300

    Add UseEditor, useHistory and useLoadstate hooks

---
+++ features/editors/hooks/use-load-state.js
@@ -0,0 +1,39 @@
+import { fabric } from "fabric";
+import { useEffect, useRef } from "react";
+import { JSON_KEYS } from "../types";
+
+// import { JSON_KEYS } from "@/features/editor/types";
+
+export const useLoadState = ({
+  canvas,
+  autoZoom,
+  initialState,
+  canvasHistory,
+  setHistoryIndex,
+}) => {
+  const initialized = useRef(false);
+
+  useEffect(() => {
+    if (!initialized.current && initialState?.current && canvas) {
+      const data = JSON.parse(initialState.current);
+
+      canvas.loadFromJSON(data, () => {
+        const currentState = JSON.stringify(
+          canvas.toJSON(JSON_KEYS),
+        );
+
+        canvasHistory.current = [currentState];
+        setHistoryIndex(0);
+        autoZoom();
+      });
+      initialized.current = true;
+    }
+  }, 
+  [
+    canvas,
+    autoZoom,
+    initialState, // no need, this is a ref
+    canvasHistory, // no need, this is a ref
+    setHistoryIndex, // no need, this is a dispatch
+  ]);
+};
