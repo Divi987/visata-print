82214df69a65c530fb2ad84a104235760acdadb4
Div685 <divyeshpatel685@gmail.com>
2024-12-07 01:38:40 +0300

    Add features dir to tailwind content
    - Update Nav, tool-sidebar and sidebar components

--- features/editors/components/sidebar.jsx
+++ features/editors/components/sidebar.jsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { ImageIcon, LayoutTemplate, 
+  Pencil, Presentation, Settings, Shapes, Sparkles, Type } from 'lucide-react';
+
 // import { 
 //   LayoutTemplate,
 //   ImageIcon,
@@ -11,7 +14,7 @@
 // } from "lucide-react";
 
 // // import { ActiveTool } from "@/features/editor/types";
-// import { SidebarItem } from "./sidebar-item";
+import { SidebarItem } from "./sidebar-item";
 
 
 // export const Sidebar = ({
@@ -68,6 +71,22 @@
 //   );
 // };
 
-export const Sidebar = () => {
-  return <aside className="bg-white flex flex-col w-[100px] h-full border-r overflow-y-auto">Sidebar</aside>;
+export const Sidebar = ({ activeTool, onChangeActiveTool }) => {
+  return (
+    <aside className="bg-white flex flex-col w-[100px] h-full border-r overflow-y-auto">
+       <ul className="flex flex-col">
+        <SidebarItem
+          icon={LayoutTemplate}
+          label="Design"
+          isActive={activeTool === 'templates'}
+          onClick={() => onChangeActiveTool('templates')}
+        />
+          <SidebarItem icon={ImageIcon} label="Image" isActive={activeTool === 'images'} onClick={() => onChangeActiveTool('images')} />
+        <SidebarItem icon={Type} label="Text" isActive={activeTool === 'text'} onClick={() => onChangeActiveTool('text')} />
+        <SidebarItem icon={Shapes} label="Shapes" isActive={activeTool === 'shapes'} onClick={() => onChangeActiveTool('shapes')} />
+        <SidebarItem icon={Sparkles} label="AI" isActive={activeTool === 'ai'} onClick={() => onChangeActiveTool('ai')} />
+        <SidebarItem icon={Settings} label="Settings" isActive={activeTool === 'settings'} onClick={() => onChangeActiveTool('settings')} />
+        </ul>
+    </aside>
+  ) 
 };
---
+++ features/editors/components/tool-sidebar-header.jsx
@@ -0,0 +1,17 @@
+export const ToolSidebarHeader = ({
+    title,
+    description
+  }) => {
+    return (
+      <div className="p-4 border-b space-y-1 h-[68px]">
+        <p className="text-sm font-medium">
+          {title}
+        </p>
+        {description && (
+          <p className="text-xs text-muted-foreground">
+            {description}
+          </p>
+        )}
+      </div>
+    );
+  };
