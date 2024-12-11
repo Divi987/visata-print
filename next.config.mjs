f28348de2e552a57a08da38d0fe4ebe1644b9477
Div685 <divyeshpatel685@gmail.com>
2024-12-11 15:08:00 +0300

    Add unsplash and hono package

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'images.unsplash.com',
+          port: '',
+        },
+      ],
         domains: ['res.cloudinary.com', 'cms.cloudinary.vpsvc.com'],
         
       },
